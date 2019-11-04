eda4dd8c478cd83c5a4a383fd2d387c540bb6168
Terry <Terry_cy@hotmail.com>
2019-11-04 14:34:55 +0000

    Added tiles, remittance view and single remittance item. Need to finish subtotals for remittance page

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const cookieParser = require('cookie-parser')
 const contractData = require('./app/data/filters/contract-numbers')
 const descriptionData = require('./app/data/filters/contract-descriptions')
 const statementData = require('./app/data/statements/statement-v1.0.0.json')
+const remittanceData = require('./app/data/statements/statement-v2.0.0.json')
 const summaryData = require('./app/data/summary tables/summary-v1.0.0.json')
 
 // Run before other code to make sure variables from .env are available
@@ -67,6 +68,8 @@ var gtmId = process.env.GOOGLE_TAG_MANAGER_TRACKING_ID
 const contractContent = Object.assign(contractData)
 const descriptionContent = Object.assign(descriptionData)
 const statementContent = Object.assign(statementData)
+const remittanceContent = Object.assign(remittanceData)
+
 const summaryContent = Object.assign(summaryData)
 
 useHttps = useHttps.toLowerCase()
@@ -192,6 +195,7 @@ app.locals.extensionConfig = extensions.getAppConfig()
 app.locals.descriptionContent = descriptionContent
 app.locals.contractContent = contractContent
 app.locals.statementContent = statementContent
+app.locals.remittanceContent = remittanceContent
 app.locals.summaryContent = summaryContent
 app.locals.summaryTotal = config.summaryTotal
 
