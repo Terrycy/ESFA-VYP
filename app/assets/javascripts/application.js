d9ac4c936581fbe05f39501451fe1d887c070074
Julien Cresus <julien.cresus@methods.co.uk>
2020-04-23 11:47:45 +0100

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,3 +72,63 @@ function switchtable3() {
   }
   
 }
+
+function switchfilter() {
+  var filters1 = document.getElementById("filters1");
+  var filters2 = document.getElementById("filters2");
+  var filters3 = document.getElementById("filters3");
+
+  if (filters1.classList.contains('govuk-visually-hidden')) {
+    filters1.classList.remove("govuk-visually-hidden");
+    filters2.classList.add("govuk-visually-hidden");
+    filters3.classList.add("govuk-visually-hidden");
+  }
+  else {
+    filters1.classList.add("govuk-visually-hidden");
+  }
+
+  if (filters2.classList.contains('govuk-visually-hidden')||(filters3.classList.contains('govuk-visually-hidden')))
+  {
+    filters1.classList.remove("govuk-visually-hidden");
+  }
+  
+}
+function switchfilter2() {
+  var filters1 = document.getElementById("filters1");
+  var filters2 = document.getElementById("filters2");
+  var filters3 = document.getElementById("filters3");
+
+  if (filters2.classList.contains('govuk-visually-hidden')) {
+    filters2.classList.remove("govuk-visually-hidden");
+    filters1.classList.add("govuk-visually-hidden");
+    filters3.classList.add("govuk-visually-hidden");
+  }
+  else {
+    filters2.classList.add("govuk-visually-hidden");
+  }
+  if (transactiontable1.classList.contains('govuk-visually-hidden')||(filters3.classList.contains('govuk-visually-hidden')))
+  {
+    filters2.classList.remove("govuk-visually-hidden");
+  }
+  
+}
+function switchfilter3() {
+  var filters1 = document.getElementById("filters1");
+  var filters2 = document.getElementById("filters2");
+  var filters3 = document.getElementById("filters3");
+
+  if (filters3.classList.contains('govuk-visually-hidden')) {
+    filters3.classList.remove("govuk-visually-hidden");
+    filters2.classList.add("govuk-visually-hidden");
+    filters1.classList.add("govuk-visually-hidden");
+  }
+  else {
+    filters3.classList.add("govuk-visually-hidden");
+  }
+
+  if (filters1.classList.contains('govuk-visually-hidden')||(filters2.classList.contains('govuk-visually-hidden')))
+  {
+    filters3.classList.remove("govuk-visually-hidden");
+  }
+  
+}
