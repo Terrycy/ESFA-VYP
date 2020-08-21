230685b9a02a2a9b362f8117680dc53fbe7e49dc
Julien Cresus <julien.cresus@methods.co.uk>
2020-08-21 12:33:28 +0100

    Cleaned + 3.0.3 new

--- app/routes.js
+++ app/routes.js
@@ -40,13 +40,6 @@ router.get('/v3/filter-and-export', function (req, res) {
   res.render('v3.0.0/transaction-view/index')
 })
 
-router.get('/payment-history/remittance-statement/:remittanceDate/:remittanceId', function (req, res) {
-
-  res.render('v3.0.0/remittance-statement/index', {
-    'remittanceDate': req.params.remittanceDate,
-    'remittanceId': req.params.remittanceId
-  })
-})
 // v3.0.1 stuff
 router.get('/v3.0.1/vyp', function (req, res) {
   res.render('v3.0.1/index')
@@ -66,7 +59,7 @@ router.get('/v3.0.1/filter-and-export', function (req, res) {
 
 router.get('/payment-history/remittance-statement/:remittanceDate/:remittanceId', function (req, res) {
 
-  res.render('v3.0.1/remittance-statement/index', {
+  res.render('v3.0.3/remittance-statement/index', {
     'remittanceDate': req.params.remittanceDate,
     'remittanceId': req.params.remittanceId
   })
@@ -87,13 +80,53 @@ router.get('/v3.0.2/mvp/payment-history', function (req, res) {
 router.get('/v3.0.2/filter-and-export', function (req, res) {
   res.render('v3.0.2/transaction-view/index')
 })
-
-router.get('/payment-history/remittance-statement/:remittanceDate/:remittanceId', function (req, res) {
-
-  res.render('v3.0.1/remittance-statement/index', {
-    'remittanceDate': req.params.remittanceDate,
-    'remittanceId': req.params.remittanceId
-  })
+// v3.0.3 stuff
+router.get('/v3.0.3/vyp', function (req, res) {
+  res.render('v3.0.3/index')
+})
+router.get('/3.0.3/vyp/b', function (req, res) {
+  res.render('split-journey/index')
+})
+router.get('/v3.0.3/payment-history', function (req, res) {
+  res.render('v3.0.3/remittances/index')
+})
+router.get('/v3.0.3/mvp/payment-history', function (req, res) {
+  res.render('v3.0.3/remittances/index-alternate')
+})
+router.get('/v3.0.3/filter-and-export', function (req, res) {
+  res.render('v3.0.3/transaction-view/index')
+})
+// v3.0.4 stuff
+router.get('/v3.0.4/vyp', function (req, res) {
+  res.render('v3.0.4/index')
+})
+router.get('/3.0.4/vyp/b', function (req, res) {
+  res.render('split-journey/index')
+})
+router.get('/v3.0.4/payment-history', function (req, res) {
+  res.render('v3.0.4/remittances/index')
+})
+router.get('/v3.0.4/mvp/payment-history', function (req, res) {
+  res.render('v3.0.4/remittances/index-alternate')
+})
+router.get('/v3.0.4/filter-and-export', function (req, res) {
+  res.render('v3.0.4/transaction-view/index')
+})
+// v3.0.5 stuff
+router.get('/v3.0.5/vyp', function (req, res) {
+  res.render('v3.0.5/index')
+})
+router.get('/3.0.5/vyp/b', function (req, res) {
+  res.render('split-journey/index')
+})
+router.get('/v3.0.5/payment-history', function (req, res) {
+  res.render('v3.0.5/remittances/index')
+})
+router.get('/v3.0.5/mvp/payment-history', function (req, res) {
+  res.render('v3.0.5/remittances/index-alternate')
+})
+router.get('/v3.0.5/filter-and-export', function (req, res) {
+  res.render('v3.0.5/transaction-view/index')
 })
 
 
